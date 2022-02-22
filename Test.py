accbab3bbebee57a60dcdc222bd2e4de3a5363f3
Tessa131313 <100149165+Tessa131313@users.noreply.github.com>
2022-02-22 10:01:51 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,11 +6,11 @@ from datetime import datetime
 #Produktname
 M_SYMBOL = "SEAT"
 #Menge der Ident-Points
-C_IdentPoints = 10
+C_IdentPoints = 4
 #Ident Points Simulieren?
 S_IdentPoints = True
 #Ident Point Pattern
-P_IdentPoints = [1,3,3,3,2]
+P_IdentPoints = [1,3,3,2]
 #Ident Points Id-Startwert
 IP_id = 38462
 #Ident Point Basis Name
@@ -18,16 +18,17 @@ IP_NameBase = "TestNode"
 #M_CARR_QUANT
 M_CARR_QUANT = 1
 #Part Menge
-C_Parts = 10
+C_Parts = 4
 #Part Basis Name
 Part_NameBase = "TestPart"
 #Unit Type Pattern
 P_UnitTypes = [1,2]
 #Timestamp - Start
-S_Timestamp = datetime(2018, 1, 1, 20,10,5)
-print(S_Timestamp)
-#Timestamp - Intervall
-I_Timestamp = 0
+S_Timestamp = datetime.now()
+#Timestamp - Intervall in s!!!
+I_Timestamp = 10
+#global Time: Zeit wird nicht für jede PU zurükgesetzt
+globalTime = False
 def createIdentPoints(symbol,id_start):
     commands = []
     if (C_IdentPoints % (len(P_IdentPoints)) != 0):
@@ -65,14 +66,58 @@ def createProductionUnits(M_Symbol, M_PU_IDENT, TypePattern):
                 }
             )
     return commands
-def simulateIdentPoints(P_Symbol, PU_Symbol, IP_Symbol, ACTION_Pattern, M_CARR_QUANT, M_TIMESTAMP):
-    print("hello")
-def returnJSON(IdentPoints, Parts):
+def simulateIdentPoints(P_Symbol, PU_Symbol, IP_Symbol, ACTION_Pattern, M_CARR_QUANT, M_TIMESTAMP, C_IdentPoints, I_TIMESTAMP, C_Parts, globalTime, S_IdentPoints):
+    if (C_IdentPoints % len(ACTION_Pattern) != 0):
+        return "Invalid Pattern length"
+    if (M_CARR_QUANT <= 0):
+        return "Invalid Product Amount"
+    if (S_IdentPoints == False):
+        return "Ident-Point simulation not active"
+    commands = []
+    time = M_TIMESTAMP
+    for c in range (0, C_Parts):
+        if (globalTime != True):
+            time = M_TIMESTAMP
+        for i in range (0, C_IdentPoints):
+            m = time.timestamp()
+            m = m + I_TIMESTAMP
+            time = datetime.fromtimestamp(m)
+            if (i < len(ACTION_Pattern)):
+                commands.append(
+                {
+                "M_SYMBOL": P_Symbol,
+                "M_PU_IDENT": PU_Symbol + str(c),
+                "M_IDENTPOINT_Symbol": IP_Symbol + str(i),
+                "M_ACTION": ACTION_Pattern[i],
+                "M_CARR_QUANT": M_CARR_QUANT,
+                "M_TIMESTAMP": str(time)
+                }
+                )
+            else:
+                e = i
+                while (e>len(ACTION_Pattern)-1):
+                    e -= len(ACTION_Pattern)
+                commands.append(
+                    {
+                    "M_SYMBOL": P_Symbol,
+                    "M_PU_IDENT": PU_Symbol + str(c),
+                    "M_IDENTPOINT_Symbol": IP_Symbol + str(i),
+                    "M_ACTION": ACTION_Pattern[e],
+                    "M_CARR_QUANT": M_CARR_QUANT,
+                    "M_TIMESTAMP": str(time)
+                    }
+                )
+    return commands
+def returnJSON(IdentPoints, Parts, Simulation):
     k = {
         "IdentPointCreatingCommands":IdentPoints,
-        "ProductionUnitCreatingCommands":Parts
+        "ProductionUnitCreatingCommands":Parts,
+        "IdentPointSimulation": Simulation
     }
-    print(json.dumps(k))
+    file = open("result.json", "w")
+    file.write(json.dumps(k))
+    
 a = createIdentPoints(IP_NameBase, IP_id)
 b = createProductionUnits(M_SYMBOL, Part_NameBase, P_UnitTypes)
-returnJSON(a,b)
+c = simulateIdentPoints(M_SYMBOL, Part_NameBase, IP_NameBase, P_IdentPoints, M_CARR_QUANT, S_Timestamp, C_IdentPoints, I_Timestamp, C_Parts, globalTime, S_IdentPoints)
+returnJSON(a,b,c)
